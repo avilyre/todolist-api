b65642736cbd66f20b9befa03a279fad876a1e50
Avily Silva <avilyre@gmail.com>
2024-07-16 13:34:07 -0300

    chore: add lombok

---
+++ src/main/java/com/avilysilva/todolist/users/UserController.java
@@ -0,0 +1,17 @@
+package com.avilysilva.todolist.users;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @PostMapping("/")
+    public String create(@RequestBody UserModel userModel) {
+        return userModel.getUsername();
+    }
+
+}
---
+++ src/main/java/com/avilysilva/todolist/users/UserModel.java
@@ -0,0 +1,12 @@
+package com.avilysilva.todolist.users;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserModel {
+    private String name;
+    private String username;
+    private String password;
+}
