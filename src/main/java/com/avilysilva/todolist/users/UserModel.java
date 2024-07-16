b44b5d59c1d83b4a96abd5aa547682a298e00534
Avily Silva <avilyre@gmail.com>
2024-07-16 15:35:38 -0300

    chore: add integration with h2 database

--- src/main/java/com/avilysilva/todolist/users/UserModel.java
+++ src/main/java/com/avilysilva/todolist/users/UserModel.java
@@ -1,12 +1,28 @@
 package com.avilysilva.todolist.users;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 
-@Getter
-@Setter
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data // Or @Getter and @Setter
+@Entity(name = "tb_users")
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    // @Column(name = "<custom-column-name>")
     private String name;
     private String username;
     private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt; // will be automatically understood by database and converted to created_at
+
 }
